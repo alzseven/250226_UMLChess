8db618f4e4f1f9417ac9e4b3d68e59c6a273f816
ParkHongJ <qkrghdwns98@naver.com>
2025-02-28 16:31:21 +0900

    1차 병합
    Board 헤더파일 분리하고 병합

---
+++ 250226_UMLChess/Board.h
@@ -0,0 +1,52 @@
+#pragma once
+#include "ChessData.h"
+class Unit;
+class Rook;
+
+class Board
+{
+public:
+	~Board()
+	{
+		for (int i = 0; i < UNIT_SIZE; i++)
+		{
+			delete Units[i];
+		}
+		delete[] Units;
+	}
+public:
+	void Init();
+	
+	bool MoveUnit(int FromX, int FromY, Team Team, int ToX, int ToY);
+	
+
+	Unit** GetUnits();
+
+	// "내가 이동을 마친 후" 체크메이트 검사니까 *****상대가 체크메이트 상태인지***** 검사하는 함수.
+	bool CheckMate(Team currentTeam);
+
+	Team GetGridInfo(int x, int y);
+	
+private:
+	bool CanMove(int x, int y);
+
+	//Attacker로부터 공격을 몸빵할 Team유닛이 있는지
+	bool CanBlockCheck(int kingX, int kingY, Unit& Attacker, Team Team);
+
+	// 특정 좌표가 Team 유닛들에 의한 공격 대상인지 확인하는 함수
+	bool CanMoveUnits(int targetX, int targetY, Team Team);
+
+
+	void FindAttackingUnit(int kingX, int kingY, Team enemyTeam, Unit** attacker);
+
+private:
+	Unit** Units;
+
+	//현재 유닛검사 로직상 King은 담아두는게 좋을 것 같다
+	Unit* Kings[2] = { nullptr };
+
+	const int UNIT_SIZE = 32;
+	const int BOARD_HEIGHT = 8;
+	const int BOARD_WIDTH = 8;
+	bool bEndGame = false;
+};
--- 250226_UMLChess/Unit.h
+++ 250226_UMLChess/Unit.h
@@ -1,26 +1,38 @@
-//#pragma once
-//#include <string>
-//#include "ChessData.h"
-//
-//class Unit
-//{
-//public:
-//	Unit(std::string name = "", char symbol = '.', Team t = Team::WHITE, int x = 0, int y = 0);
-//
-//	std::string GetName() const;
-//	int GetX() const;
-//	int GetY() const;
-//	bool IsDead() const;
-//	char GetSymbol() const;
-//	Team GetTeam() const;
-//	void SetDead(bool bDead);
-//	void Move(int x, int y);
-//	virtual bool CanMove(int x, int y, class Board*);
-//protected:
-//	std::string Name;
-//	char Symbol;
-//	Team team;
-//	int x;
-//	int y;
-//	bool bDead;
-//};
+#pragma once
+#include <string>
+#include "ChessData.h"
+
+class Unit
+{
+public:
+	Unit(std::string name = "", char symbol = '.', Team t = Team::WHITE, int x = 0, int y = 0);
+
+	std::string GetName() const;
+	int GetX() const;
+	int GetY() const;
+	bool IsDead() const;
+	char GetSymbol() const;
+	Team GetTeam() const;
+	void SetDead(bool bDead);
+	void Move(int x, int y);
+	virtual bool CanMove(int x, int y, class Board*);
+protected:
+	std::string Name;
+	char Symbol;
+	Team team;
+	int x;
+	int y;
+	bool bDead;
+};
+
+class Rook : public Unit
+{
+public:
+	Rook(std::string name, char symbol, Team team, int x, int y)
+		: Unit(name, symbol, team, x, y) 
+	{
+
+	}
+
+	virtual bool CanMove(int x, int y, class Board* chessBoard) override;
+};

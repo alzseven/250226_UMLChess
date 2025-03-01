4b35ce01bbcc6cb65792f56a4d89e794934e3e29
alzseven <jhp7140@gmail.com>
2025-03-01 23:47:50 +0900

    Disable external gamemanager class

--- 250226_UMLChess/GameManager.h
+++ 250226_UMLChess/GameManager.h
@@ -10,35 +10,35 @@
 #include "Unit.h"
 
 
-
-class GameManager
-{
-private:
-	Board chessBoard;
-	Team CurrentTeam = Team::WHITE;
-
-	struct Command {
-		char Letter;
-		int Number;
-	};
-	std::queue<Command> Commands;
-	bool isReplaying = false;
-
-	/*ChessSaveLoadManager fileManager;
-	ChessSaveData data;*/
-
-	bool isGameRunning = false;
-public:
-	~GameManager();
-	void Replay();
-	void Load(const char* filePath);
-	void Save(const char* filePath);
-	void Init();
-	void Update();
-private:
-	int LetterToNumber(char letter);
-	bool isValidInput(int input);
-	bool isValidInput(char input);
-	void ProcessInput(int targetX, int targetY, int destX, int destY);
-	void Render();
-};
+//
+//class GameManager
+//{
+//private:
+//	Board chessBoard;
+//	Team CurrentTeam = Team::WHITE;
+//
+//	struct Command {
+//		char Letter;
+//		int Number;
+//	};
+//	std::queue<Command> Commands;
+//	bool isReplaying = false;
+//
+//	/*ChessSaveLoadManager fileManager;
+//	ChessSaveData data;*/
+//
+//	bool isGameRunning = false;
+//public:
+//	~GameManager();
+//	void Replay();
+//	void Load(const char* filePath);
+//	void Save(const char* filePath);
+//	void Init();
+//	void Update();
+//private:
+//	int LetterToNumber(char letter);
+//	bool isValidInput(int input);
+//	bool isValidInput(char input);
+//	void ProcessInput(int targetX, int targetY, int destX, int destY);
+//	void Render();
+//};
